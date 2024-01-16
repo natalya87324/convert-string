a621bce143ac884ad7ddbc5118f28d97d56be114
Natalya <kim87324@yandex.ru>
2024-01-16 20:20:20 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function titleCase(str) {
+  const words = str.toLowerCase().split(' ');
+  
+  for (i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+  
+  return words.join(' ');
+}
+
+
+function add() {
+  let data = titleCase(document.querySelector('.add').value);
+
+  document.querySelector('.out').value = data;
+}
+
+
+
